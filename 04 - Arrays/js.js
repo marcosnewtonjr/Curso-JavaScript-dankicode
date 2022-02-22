22b1eca45de1220353e9d01791d714c82f78cc6f
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-02-22 10:19:36 -0300

    Arrays

---
+++ 04 - Arrays/js.js
@@ -0,0 +1,30 @@
+// Arrays são variáveis de múltiplos valores
+
+/*
+var nome1 = 'Aylla'
+var nome2 = 'Camila'
+var nome3 = 'Marcos'
+var nome4 = 'Maria Alice'
+*/
+
+// Criando uma nova array
+
+var nomes = ['Camila', 'Marcos']
+console.log(nomes)
+
+// Adicionando novos itens no início da array
+
+nomes.unshift('Aylla')
+console.log(nomes)
+
+// Adicionando novos itens no final da array
+
+nomes.push('Maria Alice')
+console.log(nomes)
+
+// Para acessar uma posição dentro da Array utilizamos [] e a posição começando do zero
+
+console.log(nomes[0])
+console.log(nomes[1])
+console.log(nomes[2])
+console.log(nomes[3])
