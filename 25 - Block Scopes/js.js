21e3a794fbc86ff39ea3781995cf467d114a4745
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-02-23 23:37:04 -0300

    Block Scopes

---
+++ 25 - Block Scopes/js.js
@@ -0,0 +1,11 @@
+// Utilizamos o comando let para criar uma variável dentro de um determinado escopo
+
+{
+    let nome = 'Marcos'
+    console.log(nome)
+}
+
+// Ao utilizar o let na raiz do código ele se torna uma variável global, até que seja criada outra let de mesmo nome
+
+let nome = 'Aylla'
+console.log(nome) 
