449e6004994b6c0f1d5949ec14f13bc6e364bfb5
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-02-25 23:48:25 -0300

    Destructure

---
+++ 27 - Destructure/js.js
@@ -0,0 +1,10 @@
+const arr = ['Marcos', 'Aylla', 'Maria Alice','Camila']
+
+const [a,b,c,d] = arr
+
+console.log(a)
+console.log(b)
+console.log(c)
+console.log(d)
+
+// Com esta técnica podemos desestruturar uma array e salvar cada elemento dentro de uma variável ou constante
