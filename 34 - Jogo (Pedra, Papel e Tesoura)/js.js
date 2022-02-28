edc40172f48c559c2a86506d4c3266b6a0b20259
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-02-27 23:10:18 -0300

    Criando Randon Inimigo e Guardando Informação

--- 34 - Jogo (Pedra, Papel e Tesoura)/js.js
+++ 34 - Jogo (Pedra, Papel e Tesoura)/js.js
@@ -1,5 +1,7 @@
 var els = document.querySelectorAll('.playerOptions div > img')
+var enemyOptions = document.querySelectorAll('.enemyOptions div > img')
 var playOpt = ''
+var enemyOpt = ''
 
 function resetOpacityPlayer() {
     for (var i = 0; i < els.length; i++) {
@@ -7,11 +9,29 @@ function resetOpacityPlayer() {
     }
 }
 
+function resetOpacityEnemy() {
+    for (var i = 0; i < enemyOptions.length; i++) {
+        enemyOptions[i].style.opacity = 0.3
+    }
+}
+
+function inimigoJogar() {
+    var rand = Math.floor(Math.random()*3)
+    resetOpacityEnemy()
+    enemyOptions[rand].style.opacity = 1
+    enemyOpt = enemyOptions[rand].getAttribute('opt')
+    alert(enemyOpt)
+    
+}
+
 for (var i = 0; i < els.length; i++) {
     els[i].addEventListener('click', function(t){
         resetOpacityPlayer()
         t.target.style.opacity = 1
         playOpt = t.target.getAttribute('opt')
-        alert(playOpt)
+        
+        // alert(playOpt)
+
+        inimigoJogar()
     })
 }
