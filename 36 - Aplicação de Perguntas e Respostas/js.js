8b9330d4452e438864ba73502421dacb6f869f1f
Marcos Newton <65315369+marcosnewtonjr@users.noreply.github.com>
2022-02-28 15:35:06 -0300

    Sistema de Validação das Respostas

--- 36 - Aplicação de Perguntas e Respostas/js.js
+++ 36 - Aplicação de Perguntas e Respostas/js.js
@@ -5,9 +5,31 @@ for(var i = 0; i < elementos.length; i++) {
         //alert('Alguma alteração')
         let marcado = e.target.value
         if( marcado == 'correta') {
-            alert('Você acertou!')
+            //alert('Você acertou!')
+            //let el = e.target.previousElementSibling.style.backgroundColor = 'green' // Aqui se recupera o elemento anterior para aplicar alguma alteração
+            let parentNode = e.target.parentNode            
+            parentNode.style.backgroundColor = 'green'
+
+            let els = parentNode.parentNode.querySelectorAll('.resposta')
+
+            for(var n = 0; n < els.length; n++) {
+                els[n].disabled = true // Desabilita o bloco, não permite a edição mas continua visivel
+            }
+
         } else if (marcado == 'incorreta'){
-            alert('Você errou!')
+            //alert('Você errou!')
+            //let el = e.target.previousElementSibling.style.backgroundColor = 'red' // Aqui se recupera o elemento anterior para aplicar alguma alteração
+            let parentNode = e.target.parentNode            
+            parentNode.style.backgroundColor = 'red'
+
+            let els = parentNode.parentNode.querySelectorAll('.resposta')
+
+            for(var n = 0; n < els.length; n++) {
+                els[n].disabled = true // Desabilita o bloco, não permite a edição mas continua visivel
+            }
+
+            let correta = parentNode.parentNode.querySelector('[value=correta')
+            correta.parentNode.style.backgroundColor = 'green'
         }
     })
 }
